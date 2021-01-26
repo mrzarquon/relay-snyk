e1d6b7faa634b7a12e8fa40f948b2ea4eeaf7def
Chris Barker <57210+mrzarquon@users.noreply.github.com>
2021-01-26 22:13:31 +0000

    reverse the sort

--- steps/parse/step.py
+++ steps/parse/step.py
@@ -24,7 +24,7 @@ if worst != 0:
     keys = ['id','issueType','pkgName','priorityScore']
     res = dict((k, worst[k]) for k in keys if k in worst)
     keys2 = ['severity','url','exploitMaturity','publicationTime','CVSSv3','cvssScore','identifiers']
-    summary = dict((k, worst['issueData'][k]) for k in keys if k in worst['issueData'])
+    summary = dict((k, worst['issueData'][k]) for k in keys2 if k in worst['issueData'])
     summary.update(res)
 
     logging.info("the worst summary {}".format(json.dumps(summary, indent=4, sort_keys=True)))
