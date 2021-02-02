38ffd76b894d448c98b0a9df1afe7d97485c1f72
Chris Barker <57210+mrzarquon@users.noreply.github.com>
2021-02-02 19:01:18 +0000

    return to basics

--- steps/parse/step.py
+++ steps/parse/step.py
@@ -8,25 +8,8 @@ logging.getLogger().setLevel(logging.INFO)
 
 relay = Interface()
 
-project = relay.get(D.project)
-
-worst = relay.get(D.worst)
-
-secret = relay.get(D.secret)
+project = relay.get(D.event)
 
 formatted_json = json.dumps(project, indent=4, sort_keys=True)
 
 logging.info("project info from snyk \n{}".format(formatted_json))
-
-logging.info("example secret {}".format(secret))
-
-if worst != 0:
-    keys = ['id','issueType','pkgName','priorityScore']
-    res = dict((k, worst[k]) for k in keys if k in worst)
-    keys2 = ['severity','url','exploitMaturity','publicationTime','CVSSv3','cvssScore','identifiers']
-    summary = dict((k, worst['issueData'][k]) for k in keys2 if k in worst['issueData'])
-    summary.update(res)
-
-    logging.info("the worst summary {}".format(json.dumps(summary, indent=4, sort_keys=True)))
-
-relay.outputs.set("outputkey","This will be the value of outputkey")
